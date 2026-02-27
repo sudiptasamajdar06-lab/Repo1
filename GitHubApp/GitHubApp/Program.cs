8ee5e6fdff99fc8f39e2a3d93202552bed52c4df
sudip <sudip@SUDIPTAPC>
2026-02-27 18:31:59 +0530

    undo

--- GitHubApp/GitHubApp/Program.cs
+++ GitHubApp/GitHubApp/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Change1");
-Console.WriteLine("Change2");
-Console.WriteLine("Change3");
